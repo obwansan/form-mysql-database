a1364f036f4d7f13e4acb224f48ee85e062f4242
obwansan <kob123@hotmail.co.uk>
2018-03-14 08:12:41 +0000

    Added comments and tidied up

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 
 var_dump($_POST);
 
+// Assign new instance of PDO class to $db variable
 $db = new PDO('mysql:host=127.0.0.1;dbname=MySQLTestDb', 'root');
 
+// The PDO object class has a setAttribute() method
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+// Create the query (prepared statement)
 $query = $db->prepare("INSERT INTO `adults` (`name`, `DOB`, `pet_name`, `gender`) VALUES (:name, :DOB, :pet_name, :gender);");
 
+// Bind the values entered into form to the prepared statement parameters
 $query->bindParam(':name', $_POST['name']);
 $query->bindParam(':DOB', $_POST['dob']);
 $query->bindParam(':pet_name', $_POST['petName']);
 $query->bindParam(':gender', $_POST['gender']);
 
+// Run the query
 $query->execute();
-
-//$name = $_POST['name'];
-//$dob = $_POST['dob'];
-//$petName = $_POST['petName'];
-//$gender = $_POST['gender'];
 
 ?>
 
@@ -40,9 +40,9 @@ $query->execute();
     </div>
     <div>
         <label for="male">Male</label>
-        <input id="male" type="radio" name="gender" value="m">
+            <input id="male" type="radio" name="gender" value="m">
         <label for="female">Female</label>
-        <input id="female" type="radio" name="gender" value="f">
+            <input id="female" type="radio" name="gender" value="f">
     </div>
     <div>
         <input type="submit">
